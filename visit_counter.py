c411a57f7d1bb06c6c69fbb4e26e6ddf58c84cd8
1MinCoinLab <1mincoinlab@gmail.com>
2025-06-27 16:42:37 +0900

    Update visit_counter.py

--- visit_counter.py
+++ visit_counter.py
@@ -1,1 +1,21 @@
+import os
+from datetime import datetime
 
+def get_today_count():
+    today = datetime.now().strftime("%Y-%m-%d")
+    filename = f"visits_{today}.txt"
+    
+    if not os.path.exists(filename):
+        with open(filename, 'w') as f:
+            f.write("1")
+        return 1
+    else:
+        with open(filename, 'r+') as f:
+            try:
+                count = int(f.read())
+            except ValueError:
+                count = 0
+            count += 1
+            f.seek(0)
+            f.write(str(count))
+        return count
