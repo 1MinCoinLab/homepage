51cb344a17cb46776b27340604f47a68fcffb19f
1MinCoinLab <1mincoinlab@gmail.com>
2025-07-04 10:15:13 +0900

    Update ping_wakeup.py

--- ping_wakeup.py
+++ ping_wakeup.py
@@ -1,1 +1,20 @@
+# ping_wakeup.py
 
+import threading
+import requests
+import time
+
+# 10분마다 서버에 ping 보내는 함수
+def periodic_ping():
+    while True:
+        try:
+            print("[PING] 서버에 주기적으로 요청을 보냅니다...")
+            requests.get('http://127.0.0.1:5001/')  # 배포 시엔 도메인으로 변경
+        except Exception as e:
+            print("[PING 오류]", e)
+        time.sleep(600)  # 600초 = 10분
+
+# 스레드를 시작하는 함수
+def start_ping_thread():
+    ping_thread = threading.Thread(target=periodic_ping, daemon=True)
+    ping_thread.start()
